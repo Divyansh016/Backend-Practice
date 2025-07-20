1280d59bc5409dd43c37c00b7fbf1e44ead84d23
Divyansh Agarwal <divyanshagarwal026@gmail.com>
2025-07-21 01:42:29 +0530

    github data added

--- index.js
+++ index.js
@@ -4,8 +4,34 @@ const app = express();
 
 const port = 8080;
 
+const githubData = {
+    "login": "bmizerany",
+    "id": 46,
+    "node_id": "MDQ6VXNlcjQ2",
+    "avatar_url": "https://avatars.githubusercontent.com/u/46?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/bmizerany",
+    "html_url": "https://github.com/bmizerany",
+    "followers_url": "https://api.github.com/users/bmizerany/followers",
+    "following_url": "https://api.github.com/users/bmizerany/following{/other_user}",
+    "gists_url": "https://api.github.com/users/bmizerany/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/bmizerany/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/bmizerany/subscriptions",
+    "organizations_url": "https://api.github.com/users/bmizerany/orgs",
+    "repos_url": "https://api.github.com/users/bmizerany/repos",
+    "events_url": "https://api.github.com/users/bmizerany/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/bmizerany/received_events",
+    "type": "User",
+    "user_view_type": "public",
+    "site_admin": false
+  }
+
 app.get('/', (req, res) => {
     res.send("Hello World");
+})
+
+app.get('/github', (req, res) => {
+    res.json(githubData);
 })
 
 app.get('/google', (req, res) => {
